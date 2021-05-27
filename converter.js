37a46a4b85d8e03bba4ff361d525f8b44b4de740
Afnan Shaikh <48408572+IamAfnanSk@users.noreply.github.com>
2021-05-27 14:21:13 +0530

    Removed: write invalid file to storage

--- converter.js
+++ converter.js
@@ -1,31 +1,34 @@
-const fs = require('fs');
-const XLSX = require('xlsx');
+const fs = require("fs");
+const XLSX = require("xlsx");
 
-const JSONToMedicaFormat = require('./helpers/JSONToMedicaFormat');
-const pasreToJSON = require('./helpers/pasreToJSON');
+const JSONToMedicaFormat = require("./helpers/JSONToMedicaFormat");
+const pasreToJSON = require("./helpers/pasreToJSON");
 
-const converter = (pathtofile, originalFileName = 'unNamedFile') => {
-	const workbookToFormat = XLSX.readFile(pathtofile);
+const converter = (pathtofile, originalFileName = "unNamedFile") => {
+  const workbookToFormat = XLSX.readFile(pathtofile);
 
-	const sheetToFormat = workbookToFormat.Sheets[workbookToFormat.SheetNames[0]];
+  const sheetToFormat = workbookToFormat.Sheets[workbookToFormat.SheetNames[0]];
 
-	const date = new Date();
+  const date = new Date();
 
-	const dateToAddInFileName = `${date.getUTCDate()}-${date.getUTCMonth() + 1}-${date.getUTCFullYear()}__${date.getUTCHours()}-${date.getUTCMinutes()}-${date.getUTCSeconds()}`;
+  const dateToAddInFileName = `${date.getUTCDate()}-${
+    date.getUTCMonth() + 1
+  }-${date.getUTCFullYear()}__${date.getUTCHours()}-${date.getUTCMinutes()}-${date.getUTCSeconds()}`;
 
-	const randomFileName = (Math.random() * 10000 + 1).toFixed(4);
+  const randomFileName = (Math.random() * 10000 + 1).toFixed(4);
 
-	if (sheetToFormat['A1'].v === 'H') {
-		const invoice = pasreToJSON(sheetToFormat);
+  if (sheetToFormat["A1"].v === "H") {
+    const invoice = pasreToJSON(sheetToFormat);
 
-		const formattedWorkbook = JSONToMedicaFormat(invoice);
+    const formattedWorkbook = JSONToMedicaFormat(invoice);
 
-		XLSX.writeFile(formattedWorkbook, `./public/output/valid__${dateToAddInFileName}__${randomFileName}__${invoice.number}.csv`);
+    XLSX.writeFile(
+      formattedWorkbook,
+      `./public/output/valid__${dateToAddInFileName}__${randomFileName}__${invoice.number}.csv`
+    );
 
-		fs.promises.unlink(pathtofile).catch(error => {});
-	} else {
-		fs.copyFile(pathtofile, `./public/output/invalid__${dateToAddInFileName}__${originalFileName}`, error => {});
-	}
+    fs.promises.unlink(pathtofile).catch((error) => {});
+  }
 };
 
 module.exports = converter;
